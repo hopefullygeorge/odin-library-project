b6707e8a23f6a363768a584da4a4ee65dffffe78
hopefullygeorge <159580496+hopefullygeorge@users.noreply.github.com>
2025-09-08 13:31:01 +0100

    Fix book animation

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const bookshelfDiv = document.querySelector("#bookshelfDiv");
-
-const bookCountP = document.querySelector("#bookCountP");
-const hiddenCheckbox = document.querySelector("#hiddenCheckbox");
-
-const addButton = document.querySelector("#addButton");
-const sortButton = document.querySelector("#addButton");
+const shelf = document.querySelector(".shelf");
+const bookCounter = document.querySelector("#bookCounter");
+const form = document.querySelector("#bookForm");
+const refreshButton = document.querySelector(".refreshButton");
 
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, frontCoverUrl) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
+  this.frontCoverUrl = frontCoverUrl;
   // the constructor...
 }
 
-function addBookToLibrary(title, author) {
+function addBookToLibrary(title, author, url) {
   // take params, create a book then store it in the array
-  const newBook = new Book(title, author);
+  const newBook = new Book(title, author, url);
   myLibrary.push(newBook);
 }
 
-bookCountP.innerText = myLibrary.length;
+function displayInShelf() {
+  shelf.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+    console.log(bookCard);
+    bookCard.style.backgroundImage = `url(${book.frontCoverUrl})`;
+    shelf.appendChild(bookCard);
+  });
+}
 
-// on press of addButton -> launch form.
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-//test====
+  const formData = new FormData(form);
+
+  const title = formData.get("book_title");
+  const author = formData.get("book_author");
+  const bookCover = formData.get("book_cover");
+
+  addBookToLibrary(title, author, bookCover);
+  console.table(myLibrary);
+  displayInShelf();
+});
