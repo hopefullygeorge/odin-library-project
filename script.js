2f98248744d1b14db5d43f488275fd6747d7f0bd
hopefullygeorge <159580496+hopefullygeorge@users.noreply.github.com>
2025-09-08 16:38:13 +0100

    Add functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bookCounter = document.querySelector("#bookCounter");
 const form = document.querySelector("#bookForm");
 const refreshButton = document.querySelector(".refreshButton");
 const tableBody = document.querySelector(".tableBody");
+const addButton = document.querySelector(".addBook");
 
 const myLibrary = [
   {
@@ -10,32 +11,44 @@ const myLibrary = [
     author: "J.R.R. Tolkien",
     frontCover:
       "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1546071216i/5907.jpg",
+    pages: 366,
   },
   {
     title: "Harry Potter and the Philosopher's Stone",
     author: "J.K. Rowling",
     frontCover:
       "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1598823299i/42844155.jpg",
+    pages: 333,
   },
   {
     title: "Atomic Habits",
     author: "James Clear",
     frontCover:
       "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1655988385i/40121378.jpg",
+    pages: 319,
+  },
+  {
+    title: "American Gods",
+    author: "Neil Gaiman",
+    frontCover:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1462924585i/30165203.jpg",
+    pages: 635,
   },
 ];
 
-function Book(title, author, frontCoverUrl) {
+function Book(title, author, frontCover, pages) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
-  this.frontCoverUrl = frontCoverUrl;
+  this.frontCover = frontCover;
+  this.pages = pages;
+
   // the constructor...
 }
 
-function addBookToLibrary(title, author, url) {
+function addBookToLibrary(title, author, url, pages) {
   // take params, create a book then store it in the array
-  const newBook = new Book(title, author, url);
+  const newBook = new Book(title, author, url, pages);
   myLibrary.push(newBook);
 }
 
@@ -45,7 +58,7 @@ function refreshList() {
     row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
-  <td>${book.frontCover}</td>
+  <td>${book.pages}</td>
   `;
     tableBody.appendChild(row);
   });
@@ -56,7 +69,6 @@ function createBookDiv(book) {
   newBook.classList.add("book");
   const cover = document.createElement("div");
   cover.classList.add("cover");
-
   cover.style.background = `url(${book.frontCover}) center / cover no-repeat`;
 
   const spine = document.createElement("div");
@@ -64,8 +76,10 @@ function createBookDiv(book) {
   const titleP = document.createElement("p");
   titleP.innerText = book.title;
   spine.appendChild(titleP);
+
   newBook.appendChild(spine);
   newBook.appendChild(cover);
+
   return newBook;
 }
 
@@ -79,15 +93,23 @@ function displayInShelf() {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  shelf.innerText = "";
+
   const formData = new FormData(form);
 
   const title = formData.get("book_title");
   const author = formData.get("book_author");
   const bookCover = formData.get("book_cover");
+  const pages = formData.get("page_count");
 
-  addBookToLibrary(title, author, bookCover);
+  addBookToLibrary(title, author, bookCover, pages);
   displayInShelf();
   refreshList();
 });
 
+addButton.addEventListener("click", () => {
+  form.style.display = form.style.display === "block" ? "none" : "block";
+});
+
 displayInShelf();
+refreshList();
